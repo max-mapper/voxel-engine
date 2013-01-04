a82ad76c71d69f1792c94a4117aac89b87418760
James Halliday <mail@substack.net>
2013-01-04 00:59:09 -0800

    commonjsified the lib

--- demo/blocking.js
+++ demo/blocking.js
@@ -1,12 +1,11 @@
 var voxel = require('voxel')
-var terrain = voxel.generator['Hilly Terrain']
 
 var createGame = require('../lib/game')
 
 var game = createGame({
   chunkSize: 32,
   chunks: 32,
-  generateVoxel: terrain.getVoxel.bind(terrain)
+  generateVoxel: voxel.generator['Hilly Terrain']
 })
 
 document.addEventListener('DOMContentLoaded', function() {
