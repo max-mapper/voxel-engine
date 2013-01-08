e9aa49f806c503f9f982ec4539418ae0280a8f90
Max Ogden <max@maxogden.com>
2013-01-07 23:01:14 -0800

    new demo

--- demo/demo.js
+++ demo/demo.js
@@ -17,7 +17,7 @@ var generator = function(low, high, x, y, z) {
 
 window.game = createGame({
   generateVoxelChunk: generator,
-  texturePath: '/textures/',
+  texturePath: './textures/',
   materials: ['grass', 'brick', 'dirt', 'obsidian', 'crate'],
   cubeSize: 25,
   chunkSize: 32,
@@ -37,12 +37,53 @@ blockSelector.on('select', function(material) {
   if (idx > -1) currentMaterial = idx + 1
 })
 
+game.on('collision', function (item) {
+  game.removeItem(item)
+})
+
+function createDebris (pos, value) {
+  var mesh = new THREE.Mesh(
+    new THREE.CubeGeometry(4, 4, 4),
+    game.material
+  )
+  mesh.geometry.faces.forEach(function (face) {
+    face.materialIndex = value - 1
+  })
+  mesh.translateX(pos.x)
+  mesh.translateY(pos.y)
+  mesh.translateZ(pos.z)
+  
+  return {
+    mesh: mesh,
+    size: 4,
+    collisionRadius: 22,
+    value: value
+  }
+}
+
+function explode (pos, value) {
+  for (var i = 0; i < 4; i++) {
+    var item = createDebris(pos, value)
+    item.velocity = {
+      x: (Math.random() * 2 - 1) * 0.05,
+      y: (Math.random() * 2 - 1) * 0.05,
+      z: (Math.random() * 2 - 1) * 0.05,
+    }
+    game.addItem(item)
+    setTimeout(function (item) {
+      game.removeItem(item)
+    }, 15 * 1000 + Math.random() * 15 * 1000, item)
+  }
+}
+
+
 game.appendTo('#container')
 
 game.on('mousedown', function (pos) {
   var cid = game.voxels.chunkAtPosition(pos)
   var vid = game.voxels.voxelAtPosition(pos)
   if (erase) {
+    explode(pos, game.getBlock(pos))
     game.setBlock(pos, 0)
   } else {
     game.createBlock(pos, currentMaterial)
