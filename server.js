caf0bf57f3cc1159057ad8a5334a9237706fbd5e
Max Ogden <max@maxogden.com>
2013-01-18 15:49:46 -0800

    upgrade texture and add browservefy to npm start

--- server.js
+++
@@ -1,4 +0,0 @@
-var http = require('http')
-var ecstatic = require('ecstatic')
-http.createServer(ecstatic(__dirname)).listen(8080)
-console.log('Listening on :8080, open http://localhost:8080/demo')
