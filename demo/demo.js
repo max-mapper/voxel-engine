c314b582e4190282d377e33e26009e9c9a54fe03
Duncan Angus Wilkie (mrdnk) <duncanwilkie@hotmail.com>
2013-01-20 15:40:01 +0000

    Fixed issue with changing block back to grass (inner array)

--- demo/demo.js
+++ demo/demo.js
@@ -25,6 +25,11 @@ var currentMaterial = 1
 
 blockSelector.on('select', function(material) {
   var idx = game.materials.indexOf(material)
+  if(idx === -1) {
+    for(var m = 0; m < game.materials.length; m++) {
+      if(typeof game.materials[m] === 'object' && game.materials[m][0] === material) idx = m
+    }
+  }
   if (idx > -1) currentMaterial = idx + 1
 })
 
