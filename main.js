d6060b00c74720daf4f94c2e65746c329be3491d
Mikola Lysenko <mikolalysenko@gmail.com>
2013-07-08 21:26:08 -0500

    added github ribbon

--- main.js
+++ main.js
@@ -12,17 +12,55 @@ var createVoxelMesh = require("./lib/createMesh.js")
 var glm = require("gl-matrix")
 var mat4 = glm.mat4
 
+//Load selectors
+var selectModel = document.querySelector("#selectModel")
+var showWire = document.querySelector("#showWire")
+var selectMip = document.querySelector("#selectMip")
+
+//Add options to model select
+;(function() {
+  for(var id in examples) {
+    var option = document.createElement("option")
+    option.value = id
+    option.innerHTML = id
+    selectModel.add(option)
+  }
+})()
+
 //Tile size parameters
 var TILE_SIZE = Math.floor(terrain.shape[0] / 16)|0
 
 //Config variables
 var texture, shader, mesh, wireShader
 
-function selectMesh(name) {
+function meshChange() {
+  var name = selectModel.value
   mesh = createVoxelMesh(shell.gl, name, examples[name])
   var c = mesh.center
   camera.lookAt([c[0]+mesh.radius*2, c[1], c[2]], c, [0,1,0])
 }
+
+function mipChange() {
+  var gl = shell.gl
+  switch(selectMip.value) {
+    case "none":
+      texture.magFilter = gl.NEAREST
+      texture.minFilter = gl.NEAREST
+      texture.mipSamples = 1
+    break
+    case "linear":
+      texture.magFilter = gl.LINEAR
+      texture.minFilter = gl.LINEAR_MIPMAP_LINEAR
+      texture.mipSamples = 1
+    break
+    case "aniso":
+      texture.magFilter = gl.LINEAR
+      texture.minFilter = gl.LINEAR_MIPMAP_LINEAR
+      texture.mipSamples = 4
+    break
+  }  
+}
+
 
 shell.on("gl-init", function() {
   var gl = shell.gl
@@ -31,15 +69,18 @@ shell.on("gl-init", function() {
   shader = createAOShader(gl)
   wireShader = createWireShader(gl)
   
-  //Select bunny mesh
-  selectMesh("terrain")
-  
   //Create texture atlas
   var tiles = ndarray(terrain.data,
     [16,16,terrain.shape[0]>>4,terrain.shape[1]>>4,4],
     [terrain.stride[0]*16, terrain.stride[1]*16, terrain.stride[0], terrain.stride[1], terrain.stride[2]], 0)
   texture = createTileMap(gl, tiles, 2)
-  texture.mipSamples = 4
+  
+  //Hook event listeners
+  selectMip.addEventListener("change", mipChange)
+  mipChange()
+  
+  selectModel.addEventListener("change", meshChange)
+  meshChange()
 })
 
 shell.on("gl-render", function(t) {
@@ -67,14 +108,16 @@ shell.on("gl-render", function(t) {
   gl.drawArrays(gl.TRIANGLES, 0, mesh.triangleVertexCount)
   mesh.triangleVAO.unbind()
 
-  //Bind the wire shader
-  wireShader.bind()
-  wireShader.attributes.position.location = 0
-  wireShader.uniforms.projection = projection
-  wireShader.uniforms.model = model
-  wireShader.uniforms.view = view
-  
-  mesh.wireVAO.bind()
-  gl.drawArrays(gl.LINES, 0, mesh.wireVertexCount)
-  mesh.wireVAO.unbind()
+  if(showWire.checked) {
+    //Bind the wire shader
+    wireShader.bind()
+    wireShader.attributes.position.location = 0
+    wireShader.uniforms.projection = projection
+    wireShader.uniforms.model = model
+    wireShader.uniforms.view = view
+    
+    mesh.wireVAO.bind()
+    gl.drawArrays(gl.LINES, 0, mesh.wireVertexCount)
+    mesh.wireVAO.unbind()
+  }
 })
