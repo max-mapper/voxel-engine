0cbd807927aeb72bc1bc56a08a10d43f577b5835
deathcap <deathcap@gmx.co.uk>
2014-04-03 12:36:40 -0700

    Add voxel-registry

--- demo.js
+++ demo.js
@@ -5,9 +5,11 @@ var createGUI = require('dat-gui');
 
 require('kb-bindings-ui');
 require('voxel-plugins-ui');
+require('voxel-registry');
 
 createShell({require: require, pluginOpts:
   {
+    'voxel-registry': {},
     'voxel-plugins-ui': {gui: new createGUI.GUI()},
     //'kb-bindings-ui': {gui: new createGUI.GUI()}, // TODO: add compatibility, game-shell bindings object is different than kb-bindings
   }
