7b3ed0f140b54524292fcceac8bacd00489b257c
deathcap <deathcap@gmx.co.uk>
2014-04-16 19:54:06 -0700

    Merge game-shell-voxel main.js into index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 var voxel = require('voxel')
 var ray = require('voxel-raycast')
 var control = require('voxel-control')
@@ -17,8 +18,24 @@ var regionChange = require('voxel-region-change')
 var physical = require('voxel-physical')
 var pin = require('pin-it')
 var tic = require('tic')()
+var createShell = require('gl-now')
+
+var createPlugins = require('voxel-plugins')
+require('voxel-registry')
+require('voxel-stitch')
+require('voxel-shader')
+require('voxel-mesher')
+require('game-shell-fps-camera')
 
 module.exports = Game
+
+var BUILTIN_PLUGIN_OPTS = {
+  'voxel-registry': {},
+  'voxel-stitch': {},
+  'voxel-shader': {},
+  'voxel-mesher': {},
+  'game-shell-fps-camera': {},
+};
 
 function Game(opts) {
   if (!(this instanceof Game)) return new Game(opts)
@@ -46,7 +63,7 @@ function Game(opts) {
   this.chunkDistance = opts.chunkDistance || 2
   this.removeDistance = opts.removeDistance || this.chunkDistance + 1
   
-  this.skyColor = opts.skyColor || 0xBFD1E5
+  this.skyColor = opts.skyColor || 0xBFD1E5 // TODO: ≈ clearColor array
   this.antialias = opts.antialias
   this.playerHeight = opts.playerHeight || 1.62
   this.meshType = opts.meshType || 'surfaceMesh'
@@ -64,8 +81,48 @@ function Game(opts) {
   // used to be a THREE.PerspectiveCamera set by voxel-view; see also basic-camera but API not likely compatible (TODO: make it compatible?)
   Object.defineProperty(this, 'camera', {get:function() { throw new Error('voxel-engine "camera" property removed') }})
 
-  // handles all rendering
-  this.shell = createShell({require: opts.require || require, pluginOpts: opts.pluginOpts})
+  // the game-shell
+  var shellOpts = shellOpts || {}
+  shellOpts.clearColor = opts.clearColor || [0.75, 0.8, 0.9, 1.0] // TODO: take from skyColor, packed hex
+  shellOpts.pointerLock = opts.pointerLock !== undefined ? opts.pointerLock : true
+  var shell = createShell(shellOpts)
+  shell.on('gl-init', function() {
+    // since the plugins are loaded before gl-init, the <canvas> element will be
+    // below other UI widgets in the DOM tree, so by default the z-order will cause
+    // the canvas to cover the other widgets - to allow plugins to cover the canvas,
+    // we lower the z-index of the canvas below
+    shell.canvas.style.zIndex = '-1'
+  })
+
+  shell.on('gl-error', function(err) {
+    // normally not reached; notCapable() checks for WebGL compatibility first
+    document.body.appendChild(document.createTextNode('Fatal WebGL error: ' + err))
+  })
+
+  this.shell = shell
+
+  // setup plugins
+  var plugins = createPlugins(this, {require: function(name) {
+    // we provide the built-in plugins ourselves; otherwise check caller's require, if any
+    // TODO: allow caller to override built-ins? better way to do this?
+    if (name in BUILTIN_PLUGIN_OPTS) {
+      return require(name)
+    } else {
+      return opts.require ? opts.require(name) : require(name)
+    }
+  }})
+
+  var pluginOpts = opts.pluginOpts || {}
+
+  for (var name in BUILTIN_PLUGIN_OPTS) {
+    opts.pluginOpts[name] = opts.pluginOpts[name] || BUILTIN_PLUGIN_OPTS[name]
+  }
+
+  for (var name in pluginOpts) {
+    plugins.add(name, pluginOpts[name])
+  }
+  plugins.loadAll()
+
 
   this.collideVoxels = collisions(
     this.getBlock.bind(this),
